98d12cbf15d9873fad32c27304fe8b0eba6e2139
lijun <669190@163.com>
2022-03-10 21:32:46 +0800

    test001提交

--- src/main/java/com/atguigu/pojo/Test001.java
+++ src/main/java/com/atguigu/pojo/Test001.java
@@ -4,4 +4,8 @@ public class Test001 {
     public static void main(String[] args) {
         System.out.println("args = " + args);
     }
+
+    public void test01(){
+        System.out.println("第一次提交");
+    }
 }
