c2f925087d245d05fb92edfdffaeb45ddb121b1d
lijun <669190@163.com>
2022-03-10 21:36:27 +0800

    test021第二次提交

--- src/main/java/com/atguigu/pojo/Test0021.java
+++ src/main/java/com/atguigu/pojo/Test0021.java
@@ -4,4 +4,8 @@ public class Test0021 {
     public static void main(String[] args) {
         System.out.println("第二次提交");
     }
+
+    public void test01(){
+        System.out.println("第二提交");
+    }
 }
