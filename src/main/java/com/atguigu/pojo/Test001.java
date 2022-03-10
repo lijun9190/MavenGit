a8992c66ef4ee4ea6feb6e00ae86cc26cd012ffc
lijun <669190@163.com>
2022-03-10 20:52:36 +0800

    master测试分支

---
+++ src/main/java/com/atguigu/pojo/Test001.java
@@ -0,0 +1,7 @@
+package com.atguigu.pojo;
+
+public class Test001 {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
---
+++ src/main/java/com/atguigu/pojo/Test0021.java
@@ -0,0 +1,4 @@
+package com.atguigu.pojo;
+
+public class Test0021 {
+}
